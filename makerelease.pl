10e2f002132b4acffbe6c95f6f3bb9518bb62f2d
John Beranek <jberanek@users.sourceforge.net>
2023-07-04 08:57:46 +0100

    makerelease.pl now creates containing directories again, and added a --tag option for explicitly stating the tag.

--- makerelease.pl
+++ makerelease.pl
@@ -3,14 +3,30 @@
 use warnings;
 use strict;
 
+use Getopt::Long;
+
+my $literal_tag;
+
+GetOptions('tag=s' => \$literal_tag) or die "Invalid options\n";
+
 my $rel_ver = shift or die "No version specified!\n";
 
-my $tag = "v$rel_ver";
+my $tag;
+
+if ($literal_tag)
+{
+  $tag = $literal_tag;
+}
+else
+{
+  $tag = "v$rel_ver";
+}
 
 system(
        'git',
        'archive',
        '--format=tar.gz',
+       "--prefix=mrbs-$rel_ver/",
        '-o',
        "mrbs-$rel_ver.tar.gz",
        $tag
@@ -19,6 +35,7 @@ system(
        'git',
        'archive',
        '--format=zip',
+       "--prefix=mrbs-$rel_ver/",
        '-o',
        "mrbs-$rel_ver.zip",
        $tag
